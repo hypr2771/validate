1f4f1b9702ce9a6e50a86708c0c6cf5ff8996e8c
hypr2771 <vincent.castelluci@gmail.com>
2019-04-15 14:23:33 +0200

    Update JavaDoc

--- src/main/java/io/github/hypr2771/Rules.java
+++ src/main/java/io/github/hypr2771/Rules.java
@@ -7,50 +7,54 @@ import java.util.stream.Collectors;
 
 /**
  * Rules are a set of rule used to validate a component. They will combine before returning the
- * whole set of violated rules.<br/>
+ * whole set of violated rules.
+ * <br>
  *
  * Even tho you can create a simple set of rules for your whole application doing
- * <code>Rules<?></code>, we encourage you to create a set of rules for each items.
+ * <code>Rules&lt;?&gt;</code>, we encourage you to create a set of rules for each items.
  * This will enhance generics behavior of your rules while still keeping the type safe nature
- * offered by <code>Rules</code>.<br/>
+ * offered by <code>Rules</code>.
+ * <br>
  *
- * <br/>
+ * <br>
  * <hr>
- * <br/>
+ * <br>
  *
- * The only visible method present is {@link Rules#validate(T)}, used to validate a <code>T</code>
- * type.<br/>
+ * The only visible method present is {@link Rules#validate(Object)}, used to validate a <code>T</code>
+ * type.
+ * <br>
  *
  * To implement your own set of {@link Rules}, you can simply inherit {@link Rules}, then use the
  * {@link #append(Error, Predicate)} to populate your rules in constructor.
  *
- * <br/> An example of a simple {@link Rules} implementation could be as follow :
+ * <br> An example of a simple {@link Rules} implementation could be as follow :
  *
  * <pre>
  * public class AdminRules extends {@link Rules}&lt;{@link String}&gt; {
  *
  *   public AdminRules() {
- *     this.{@link #append}(() -> "Please, at least try something smart...", target -> null != target && !target.isBlank())
- *         .{@link #append}(() -> "Not an admin", target -> target.equals("มะแว้ง"));
+ *     this.{@link #append}(() -&gt; "Please, at least try something smart...", target -&gt; null != target &amp;&amp; !target.isBlank())
+ *         .{@link #append}(() -&gt; "Not an admin", target -&gt; target.equals("มะแว้ง"));
  *   }
  *
  * }
  * </pre>
  *
- * <br/> Then you could use it as follows :
+ * <br>
+ *   Then you could use it as follows :
  *
  * <pre>
- *   private final AdminRules adminRules;
+ * private final AdminRules adminRules;
  *
- *   public String adminPanel(String name){
+ * public String adminPanel(String name){
  *
- *     var adminValidation = adminRules.{@link #validate}(name);
- *     if(adminValidation.isLeft()){
- *       return "goSu";
- *     }
- *
- *     return "lambda";
+ *   var adminValidation = adminRules.{@link #validate}(name);
+ *   if(adminValidation.isLeft()){
+ *     return "goSu";
  *   }
+ *
+ *   return "lambda";
+ * }
  * </pre>
  *
  * @param <T> the class to validate for the given set of rules
